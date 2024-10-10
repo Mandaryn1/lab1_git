eb3af6405f0f7a5a571ddf02319706d528c9f75b
Mandaryn1 <120421278+Mandaryn1@users.noreply.github.com>
2024-10-10 13:59:08 +0200

    aaa6_mam nadzieje ze nowy branchss

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
             //hbjhbjh
             //hgvhgvhgv
             //hgvhgvhgv2
-            System.out.println("co ja robie tu, uuuuuu");
+            System.out.println("co ja robie tu, uuuuuuu");
         }
     }
 }
